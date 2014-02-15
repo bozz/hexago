ac94037129816be35ffa18c23e54e2d14fc1c049
Boris Searles <boris@lucidgardens.com>
2014-02-15 22:18:30 +0100

    add minimal menu and new game button

--- public/bundle.js
+++ public/bundle.js
@@ -1,18 +1,29 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 
-var BoardView = require('./boardView.js');
+var BoardView = require('./boardView');
+    // move = require('../lib/move.min.js');
 
+
+var initGame = function() {
+    var boardView = new BoardView();
+    boardView.render();
+}
 
 document.addEventListener('DOMContentLoaded', function() {
 
-    var boardView = new BoardView();
-    boardView.render();
+    var newGameButton = document.querySelector('#menu .new-game');
+
+    newGameButton.addEventListener('click', function(e) {
+        newGameButton.disabled = true;
+        move('#menu').set('opacity', 0).duration('0.3s').then(initGame).end();
+
+    });
 
     console.log("fin.");
 });
 
 
-},{"./boardView.js":3}],2:[function(require,module,exports){
+},{"./boardView":3}],2:[function(require,module,exports){
 var Hex = require('./hex'),
     HexTile = require('./hexTile');
 
--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,22 @@
 
-var BoardView = require('./boardView.js');
+var BoardView = require('./boardView');
+    // move = require('../lib/move.min.js');
 
+
+var initGame = function() {
+    var boardView = new BoardView();
+    boardView.render();
+}
 
 document.addEventListener('DOMContentLoaded', function() {
 
-    var boardView = new BoardView();
-    boardView.render();
+    var newGameButton = document.querySelector('#menu .new-game');
+
+    newGameButton.addEventListener('click', function(e) {
+        newGameButton.disabled = true;
+        move('#menu').set('opacity', 0).duration('0.3s').then(initGame).end();
+
+    });
 
     console.log("fin.");
 });
